9d353b5bd1acd72473d0b1851e497c5f7076a665
kwan3 <92351563+kwan3@users.noreply.github.com>
2021-10-14 04:36:28 -0400

    Add files via upload

--- gm_rating.R
+++ gm_rating.R
@@ -1,49 +1,39 @@
-#Performance Grade
+#IE332 Group 17
+#Max Kwan
+#gm_rating
+#Calculates a grade for every GM
 
-#Store User Input: Draft Data
-c(draft,teams) <- read_draft_input() #User Input txt file
+#Store user inputs and collects data
+draft <- get_draft() #Get's the user inputted draft data
 current_date <- as.Date("2020-10-20") #User Input String
-current_season_stats <- get_stats(as.numeric(format(current_date, "%Y"))) #Scrape current season stats
+current_season <- get_stats(as.numeric(format(current_date, "%Y"))) #Scrape current season stats
+current_season_raw <- get_stats(as.numeric(format(current_date, "%Y")))
 past_season_stats <- get_stats(as.numeric(format(current_date, "%Y")) - 1) #Scrape past season stats
-running_draft <- merge(draft, current_season_stats, by ='Player', all = TRUE) #merge draft and current season stats, not sure if I should
+
+#Store important data
+team_names <- unique(draft$Team)
+num_teams <- length(team_names)-0
+
+#Creates the running draft
+running_draft <- merge(draft, current_season, by ='Player', all.x = TRUE) #merge draft and current season stats, not sure if I should
 
 #Ranked lists for each position
-"
-RW_ranked <- draft
-For (i in 1:length(draft)) {
-  if(draft$Pos[i] == 'RW') {
-    RW_ranked = 
-  }
-}
-"
-#team_report <- names(draft)
-pick_rating <- zeroes(length(draft)) #Initialize in case im dumb
+#draft <- data.frame("Draft Position" = (1:6), "Team Name" = paste("Team", c(1:6)), "Player Name" = c("Adam","Bob", "Carl", "Dave", "Edward", "Frank"))
 
+#Initializing Vectors
+pick_rating <- (1:length(draft$Draft.Position)) - 0
+gm_report <- data.frame("Name" = team_names, "Grade" = rep(0,num_teams))
+
+#Iterate over draft and calculate pick rating for each drafted player
 for (i in 1:length(draft)) {
-  pick_rating[i] = grep(draft$Player[i], filter(running_draft, Pos == draft$Pos[i])$Player) #Finds the index of the pick from the running list
-  running_draft[-grep(draft$Player[i],running_draft$Player)] #Removes player from running list
+  pick_rating[i] <- grep(running_draft$Player[i], filter(current_season, Pos == running_draft$Pos[i])$Player) #Finds the index of the pick from the running list
+  current_season <- current_season[-grep(running_draft$Player[i], current_season$Player),] #Removes player from running list
+  gm_report$Grade[grep(running_draft$Team[i], gm_report$Name)] <- gm_report$Grade[grep(running_draft$Team[i], gm_report$Name)] + pick_rating[i] #Adds pick rating to cumulative gm grade
 }
-
-cbind(draft, pick_rating) #combining into draft
-
-gm_rating <- draft.groupby(['Team'])$pick_rating.sum() #Save pick ratings into report
-
-#Functions
-read_draft_input <- function(location = "/Users/maxkwan/Desktop/Everything/College/Senior/IE332/Project/mock1.txt" ) {
-  #https://cran.r-project.org/doc/manuals/R-data.html#Spreadsheet_002dlike-data
-  
-  readLines("/Users/maxkwan/Desktop/Everything/College/Senior/IE332/Project/mock1.txt")
-  #Not sure quite how to format this without using grep or splitting an absurd # of times
-  #Remove rounds and set draft as absolute draft position
-  #Assume output is a beautiful table of the current season draft 
-  #Columns: Draft Position, Team, Player, Hometown
-  return c(table, c(team_names))
-}
+#Having issues with merging probably, the draft positions are not matching up with the player
 
 
-pickRating #No need to turn it into a function
-Equate to difference in rank position of current season to draft position
-Positive indicates good picks, negative indicates poor picks 
+#Stores final results
+draft <- cbind(draft, pick_rating)
 
-draft position = string[,2]
-name = grep('-', string, )
+
