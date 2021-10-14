cefbc6418a3588bd126c431a72e62244fc4f329e
kwan3 <92351563+kwan3@users.noreply.github.com>
2021-10-14 03:31:11 -0400

    Add files via upload

---
+++ get_draft.R
@@ -0,0 +1,35 @@
+#IE332 Group 17
+#Max Kwan
+#get_draft
+#Scrapes draft data from the user inputed txt file
+
+get_draft <- function(location = "/Users/maxkwan/Desktop/Everything/College/Senior/IE332/Project/mock1.txt") {
+  #location = "/Users/maxkwan/Desktop/Everything/College/Senior/IE332/Project/mock1.txt"
+  
+  #Reads in txt file and formats character array
+  raw_draft <- readLines(location)
+  raw_draft <- raw_draft[-which(raw_draft == "")]
+  num_rounds <- length(grep("Round", raw_draft, ignore.case = TRUE)) - 0
+  num_teams <- grep("Round 2", raw_draft, ignore.case = TRUE) -2
+  raw_draft <- raw_draft[-grep("Round", raw_draft, ignore.case = TRUE)]
+  
+  #Initializing Variables
+  Draft_pos <- 1:length(raw_draft)
+  Round <- 1:length(raw_draft)
+  Team <- paste("", 1:length(raw_draft))
+  Player <- paste("", 1:length(raw_draft))
+  
+  #Iterates over raw_draft pulling required data
+  for (i in 1:length(raw_draft)) { 
+    Draft_pos[i] <- i
+    Round[i] <- floor(i/num_rounds) + 1
+    Team[i] <- substring(raw_draft[i], 5, regexpr("-",raw_draft[i]) - 2)
+    ugly_name <-strsplit(strsplit(raw_draft[i], split = "\\(")[[1]][2], split="-")[[1]][2]
+    Player[i] <- substring(ugly_name, 2)
+  }
+  
+  output <- data.frame("Draft Position" = Draft_pos, "Round" = Round, "Team" = Team, "Player" = Player)
+  return(output)
+}
+
+
