3456bc7761e6e3c69c9666201502ddc553062177
kwan3 <92351563+kwan3@users.noreply.github.com>
2021-10-24 03:03:45 -0400

    Add files via upload

--- get_draft.R
+++ get_draft.R
@@ -15,20 +15,19 @@ get_draft <- function(location = "/Users/maxkwan/Desktop/Everything/College/Seni
   
   #Initializing Variables
   Draft_pos <- 1:length(raw_draft)
-  Round <- 1:length(raw_draft)
   Team <- paste("", 1:length(raw_draft))
   Player <- paste("", 1:length(raw_draft))
   
   #Iterates over raw_draft pulling required data
   for (i in 1:length(raw_draft)) { 
     Draft_pos[i] <- i
-    Round[i] <- floor(i/num_rounds) + 1
+    #Round[i] <- floor(i/num_rounds) + 1
     Team[i] <- trimws(substring(raw_draft[i], 5, regexpr("-",raw_draft[i]) - 2), which = c("both", "left", "right") )
     ugly_name <-strsplit(strsplit(raw_draft[i], split = "\\(")[[1]][2], split="-")[[1]][2]
     Player[i] <- trimws(ugly_name, which = c("both", "left", "right"))
   }
   
-  output <- data.frame("Draft Position" = Draft_pos, "Round" = Round, "Team" = Team, "Player" = Player)
+  output <- data.frame("Draft Position" = Draft_pos, "Team" = Team, "Player" = Player)
   return(output)
 }
 
